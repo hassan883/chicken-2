3e8add117fbe73d4b38e91c202d2e7775f60b4af
hassan javed <hassanjaved917127@gmail.com>
2023-06-12 21:36:09 -0700

    experiments of data Ingestion have been done

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # chicken-2
+
+
+## Workflows
+
+1. update config.yaml 
+2. update secrete.yaml [optional]
+3. update params.yaml 
+4. update the entitiy 
+5. update the configaration manager in src config 
+6. update the components 
+7. update the pipline 
+8. update the main.py 
+9. update dvc.yaml 
--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -1,47 +1,50 @@
 import os
 from box.exceptions import BoxValueError
-import yaml 
+import yaml
 from cnnClassifier import logger
-import json 
+import json
 import joblib
-from ensure import ensure_annotations 
+from ensure import ensure_annotations
 from box import ConfigBox
-from pathlib import Path 
+from pathlib import Path
 from typing import Any
 import base64
 
 
+
 @ensure_annotations
-def read_yaml(path_to_yaml:Path)->ConfigBox:
-    """read yaml file and returns 
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
 
-    Args: 
-        path_to_yaml (str): path like input 
-    Raises: 
-        ValueError: if yaml file is empty 
-        e: empyt file
+    Args:
+        path_to_yaml (str): path like input
 
-    Returns: 
-        configBox: configBox type 
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
     """
-    try: 
+    try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
-        return ValueError("yaml file is empty")
+        raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
     
 
+
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
-    """create list of directories 
+    """create list of directories
 
-    Args: 
-        path_to_directories (list): list of path directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Default to false
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
@@ -53,14 +56,17 @@ def create_directories(path_to_directories: list, verbose=True):
 def save_json(path: Path, data: dict):
     """save json data
 
-    Args: 
+    Args:
         path (Path): path to json file
         data (dict): data to be saved in json file
     """
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
-    logger.info(f"json file is saved at: {path}")
+    logger.info(f"json file saved at: {path}")
+
+
+
 
 @ensure_annotations
 def load_json(path: Path) -> ConfigBox:
@@ -129,4 +135,3 @@ def decodeImage(imgstring, fileName):
 def encodeImageIntoBase64(croppedImagePath):
     with open(croppedImagePath, "rb") as f:
         return base64.b64encode(f.read())
-
